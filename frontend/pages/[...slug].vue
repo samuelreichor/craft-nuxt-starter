10b8d23307a47856d17330c433fe85aea1bb3e22
samuelreichor <samuelreichor@gmail.com>
2024-11-21 22:51:43 +0100

    Update nuxt-craftcms and add comments to slug.vue

--- frontend/pages/[...slug].vue
+++ frontend/pages/[...slug].vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
   import { CraftNotImplemented, type Config } from 'vue-craftcms'
   import Home from '~/templates/pages/home.vue';
-  import News from '~/templates/pages/news.vue';
-/*   import Error404 from '~/templates/pages/404.vue' */
+/*  import News from '~/templates/pages/news.vue'; */
 
   import Headline from '~/templates/components/headline.vue'
 
+  // Map your Craft CMS data to your vue components and pages
   const mapping: Config = {
     pages: {
-      'home': Home,
-      'news': News,
+      'home:entry_default': Home,
+      'news:entry_default': CraftNotImplemented,
     },
     components: {
       'block_imageText': CraftNotImplemented,
+      'block_author': CraftNotImplemented,
       'block_headline': Headline,
     }
   };
 
+  // Get current uri and fetch current entry
   const route = useRoute()
   const uri = route.params.slug.length > 0 ? route.params.slug : '__home__';
   const { data, error } = await useCraftQuery('entries').uri(uri).one()
